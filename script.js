2af8b49b519c034fada0049e47e5bd0389b1c39a
77Pixels <seventysevenpixels@gmail.com>
2024-03-17 16:00:59 +0300

    completed product filter

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ menuIcon.addEventListener("click", function () {
 closeNav.addEventListener("click", function () {
   sideNav.style.right = "-50%";
 });
+
+// Product Page
+let search = document.getElementById("search");
+let productContainer = document.getElementById("product-container");
+let productList = productContainer.querySelectorAll("div");
+
+search.addEventListener("keyup", function () {
+  let enteredProduct = event.target.value.toLowerCase();
+  console.log(enteredProduct);
+  for (count = 0; count < productList.length; count = count + 1) {
+    let productName = productList[count].querySelector("h1").textContent;
+    console.log(productName);
+    console.log(productName);
+    if (productName.toLowerCase().indexOf(enteredProduct) < 0) {
+      productList[count].style.display = "none";
+    } else {
+      productList[count].style.display = "block";
+    }
+  }
+});
